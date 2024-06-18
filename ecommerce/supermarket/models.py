fb9f87845dbe693b6ed82c11cfe632d28337c555
YannickMagayane <yannickmagayaneyannick@gmail.com>
2024-06-18 10:18:53 +0200

    open cv n'est pas pris en compte

--- ecommerce/supermarket/models.py
+++ ecommerce/supermarket/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-import cv2
-import os
 from user.models import User
 from auditlog.registry import auditlog
 
@@ -15,38 +13,6 @@ class SuperMarket(models.Model):
     def __str__(self):
         return self.name
 
-    def remove_background(self):
-        if self.photo:
-            image_path = self.photo.path
-
-            # Charger l'image avec OpenCV
-            image = cv2.imread(image_path)
-
-            # Convertir l'image en niveau de gris
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            # Appliquer un seuillage pour séparer l'avant-plan de l'arrière-plan
-            _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
-
-            # Trouver les contours de l'avant-plan
-            contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-            # Créer un masque pour l'avant-plan
-            mask = cv2.drawContours(image.copy(), contours, -1, (255, 255, 255), thickness=cv2.FILLED)
-
-            # Appliquer le masque pour supprimer l'arrière-plan
-            result = cv2.bitwise_and(image, mask)
-
-            # Obtenir le nom du fichier sans extension
-            filename_without_extension = os.path.splitext(os.path.basename(image_path))[0]
-
-            # Enregistrer l'image sans arrière-plan avec le nom du supermarché
-            output_path = os.path.join(os.path.dirname(image_path), f"{filename_without_extension}_no_bg.png")
-            cv2.imwrite(output_path, result)
-
-            return output_path
-
-        return None
 
     @classmethod
     def filter_by_name(cls, name):
